dfb9d06a672cd06a0b400454cca39f45839c2198
raka <rakasandheo160304@gmail.com>
2024-05-10 17:05:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <!-- Header -->
+    <header>
+      <nav>
+        <ul>
+          <li @click="showTodos">Todos</li>
+          <li @click="showPosts">Posts</li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Main Content -->
+    <main>
+      <div v-if="activeMenu === 'todos'">
+        <!-- Fitur Todos -->
+        <h2>Todos</h2>
+        <!-- Placeholder untuk implementasi fitur Todos -->
+      </div>
+      <div v-else-if="activeMenu === 'posts'">
+        <!-- Fitur Postingan -->
+        <h2>Postingan</h2>
+        <select v-model="selectedUser">
+          <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+        </select>
+        <div v-if="selectedUser">
+          <div v-for="post in filteredPosts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+            <p>{{ post.body }}</p>
+          </div>
+        </div>
+        <div v-else>
+          <p>Silakan pilih pengguna untuk melihat postingan mereka.</p>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      activeMenu: 'todos',
+      users: [],
+      posts: [],
+      selectedUser: null
+    };
+  },
+  methods: {
+    showTodos() {
+      this.activeMenu = 'todos';
+    },
+    showPosts() {
+      this.activeMenu = 'posts';
+    }
+  },
+  mounted() {
+    // Ambil data user dari API
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        this.users = data;
+      });
+
+    // Ambil data postingan dari API
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        this.posts = data;
+      });
+  },
+  computed: {
+    filteredPosts() {
+      // Filter postingan berdasarkan user yang dipilih
+      return this.posts.filter(post => post.userId === parseInt(this.selectedUser));
+    }
+  }
+};
+</script>
